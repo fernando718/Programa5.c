28a7a41688b9ce3517933f0240e010fbd23c4748
fernando718 <82851141+fernando718@users.noreply.github.com>
2021-05-03 18:26:22 -0500

    Create Programa5.c

---
+++ Programa5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main () 
+{
+     /* Fernando Valdivia
+    Equipo umizumi
+    21 abr 21
+    Programa 4. Pide 2 valores reales, calcula multiplica, divide y muestra resultado
+    */
+
+   float primer, segundo, multiplica, division;
+    printf ("\n\n Programa 5");
+    printf ("\n\n Saca el area de un triángulo");
+    printf ("\n\n Dame el primer valor de la base ");
+    scanf ("%f",&primer); //Permite capturar un valor real y lo guarda en variable primer
+    printf ("\n\n Dame el segundo valor de la altura ");
+    scanf ("%f",&segundo); //Permite capturar un valor real y lo guarda en variable primer
+    multiplica = primer * segundo;
+    division = primer * segundo / 2
+    printf ("\n El area del triángulo %f",division);
+
+    return 0;
+}
